a47f18e1456f26d24bdca850ba3169e0fe1faa50
ASPP Student <student@localhost>
2017-08-28 18:22:29 +0300

    insulting germans

--- fun/main.py
+++ fun/main.py
@@ -9,8 +9,11 @@ def important_question():
             print('Really? You like coffee in your ice cream? Weird.')
         elif resp == 'vanilla':
             print('SO BORING.')
+        elif resp == 'fuck zou':
+            print("Oh you're german!")
         else:
             print('WRONG. YOU SUCK. TRY AGAIN.')
         resp = input('What is the best ice cream flavor? ').lower()
+        
     print('YOU HAVE EXCELLENT TASTE. NICE.')
     exit() 
