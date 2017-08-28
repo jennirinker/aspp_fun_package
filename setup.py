8fc524d255d4fa38e48e607bda00f50cd8158963
ASPP Student <student@localhost>
2017-08-28 18:40:04 +0300

    initial upload setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='fun',
+    version='0.1',
+    description='The best function you will ever use!',
+    url='http://github.com/jennirinker/fun',
+    author='Fun experts',
+    author_email='flyingcircus@example.com',
+    license='Do whatever the fuck you want with it aka BSD2',
+    packages=['fun'],
+    zip_safe=False)
