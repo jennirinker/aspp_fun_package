2ab80d773c36133ca5fcd59002b37c76f994bd2d
ASPP Student <student@localhost>
2017-08-28 18:14:58 +0300

    Improving the choices and adding autoexit

--- fun/main.py
+++ fun/main.py
@@ -3,9 +3,14 @@ def important_question():
     """
     resp = input('What is the best ice cream flavor? ').lower()
     while resp != 'coconut':
-        print('WRONG. YOU SUCK. TRY AGAIN.')
-        resp = input('What is the best ice cream flavor? ').lower()
         if resp == 'fuck you':
             print('WELL FUCK YOU!')
+        elif resp == 'tiramisu':
+            print('Really? You like coffee in your ice cream? Weird.')
+        elif resp == 'vanilla':
+            print('SO BORING.')
+        else:
+            print('WRONG. YOU SUCK. TRY AGAIN.')
+        resp = input('What is the best ice cream flavor? ').lower()
     print('YOU HAVE EXCELLENT TASTE. NICE.')
-    return
+    exit() 
